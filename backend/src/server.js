162b8c6176d811c11c92b8b360b8d030cee9bff9
Lithira-Sasmitha <lithirasasmitha@gmail.com>
2025-05-03 13:37:26 +0530

    create project

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import "dotenv/config"; 
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import { connect } from "mongoose";
+
+const app = express();
+const PORT = process.env.PORT || 5001; // Default fallback in case .env is missing
+
+
+app.use("/api/auth", authRoutes) 
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
+});
